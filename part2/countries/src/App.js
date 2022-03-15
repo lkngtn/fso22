d0752d7d55d22719532a293878e83ef269754476
lkngtn <luke@lukeduncan.net>
2022-03-15 15:51:42 -0700

    Complete exercise 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -10,7 +10,28 @@ const Filter = ({nameFilter, handleFilterChange}) => {
   )
 }
 
+const Weather = ({country}) => {
+  const [weather, setWeather] = useState({fetched: false})
+  useEffect(() => {
+    const api_key = process.env.REACT_APP_API_KEY
+    axios.get(`https://api.openweathermap.org/data/2.5/weather?lat=${country.latlng[0]}&lon=${country.latlng[1]}&units=metric&appid=${api_key}`).then(response => {
+      setWeather({success: true, temp: response.data.main.temp, icon: response.data.weather[0].icon, wind: response.data.wind.speed})
+    })
+  },[country])
+  return weather.fetched ? 
+    (
+      <>
+        <h3>Weather in {country.capital}</h3>
+        <p>Temperature: {weather.temp} Celsius</p>
+        <img src={`https://openweathermap.org/img/wn/${weather.icon}@2x.png`} alt="icon" />
+        <p>wind speed: {weather.wind} m/s </p>
+      </>
+    )
+  : <></> 
+}
+
 const Country = ({country}) => {
+ 
   return (
     <div>
       <h3> {country.name.common} {country.flag} </h3>
@@ -20,6 +41,7 @@ const Country = ({country}) => {
       <ul>
         {Object.values(country.languages).map(language => <li key={language}>{language}</li>)}
       </ul>
+      <Weather country={country} />
     </div>
   )
 }
@@ -33,7 +55,7 @@ const Countries = ({countries, nameFilter, setNameFilter}) => {
   if (filteredCountries.length === 1) {
     let country = filteredCountries[0]
     return (
-      <Country country={country} />
+      <Country country={country}/>
     )
   } else {
     return filteredCountries.map(country => <p key={country.name.official}>{country.name.common}<button onClick={() => setNameFilter(country.name.common)}>Select</button></p>)
@@ -41,26 +63,23 @@ const Countries = ({countries, nameFilter, setNameFilter}) => {
 }
 
 const App = () => {
-  const [countries, setCountries] = useState([
-  ])
-
+  const [countries, setCountries] = useState([])
   const [nameFilter, setNameFilter] = useState('')
-  const handleFilterChange = (event) => setNameFilter(event.target.value)
+  const [weather, setWeather] = useState({})
 
   useEffect(() => {
-    // https://restcountries.com/v3.1/all
-    // http://localhost:3001/countries
     axios.get('https://restcountries.com/v3.1/all').then(response => {
-      console.log('promise fulfilled')
       setCountries(response.data)
     })
   }, [])
-  
+
+  const handleFilterChange = (event) => setNameFilter(event.target.value)
+
   return (
     <div>
       <h2>Search Countries</h2>
       <Filter handleFilterChange={handleFilterChange} nameFilter={nameFilter} />
-      <Countries countries={countries} nameFilter={nameFilter} setNameFilter={setNameFilter} />
+      <Countries countries={countries} weather={weather} setWeather={setWeather} nameFilter={nameFilter} setNameFilter={setNameFilter} />
     </div>
   )
 }
