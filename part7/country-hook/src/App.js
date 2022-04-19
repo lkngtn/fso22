81f9b7d193ebd76b5703aec73e064984362d1a66
lkngtn <luke@lukeduncan.net>
2022-04-19 15:47:12 -0700

    Complete exercise 7.7

--- part7/country-hook/src/App.js
+++ part7/country-hook/src/App.js
@@ -18,7 +18,13 @@ const useField = (type) => {
 const useCountry = (name) => {
   const [country, setCountry] = useState(null)
 
-  useEffect(() => {})
+  useEffect(() => {
+    if(name) {
+      axios.get(`https://restcountries.com/v3.1/name/${name}?fullText=true`).then( response => {
+        setCountry(response.data[0])
+      }).catch( error => console.log(error))
+    }
+  }, [name])
 
   return country
 }
